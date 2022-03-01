627263e4e0597e666f981ff0e649873894110fbf
Harlan <hsavinturner@gmail.com>
2022-02-28 20:43:39 -0500

    implemented gcd function

--- codingassignment2.pl
+++ codingassignment2.pl
@@ -1,3 +1,19 @@
 #!/usr/bin/perl
-use strict;
-use warnings FATAL => 'all';
+#use strict;
+#use warnings FATAL => 'all';
+
+sub gcd
+{
+    my ($x,$y) = @_;
+    while(($x!=0) && ($y!=0))
+    {
+        if ($x>$y) {$x=$x%$y;} else{$y=$y%$x;}
+    }
+    $x+$y;
+
+}
+
+$x = 4;
+$y = 2;
+
+print gcd($x,$y);
