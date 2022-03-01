e3f22eafbe27fe9891e419c1e5b86211aa27da37
Harlan <hsavinturner@gmail.com>
2022-02-28 23:35:44 -0500

    cleaned up howmany function/rudimentary implementation of maketoggle closure

--- codingassignment2.pl
+++ codingassignment2.pl
@@ -18,14 +18,43 @@ sub gcd
 $x = 4;
 $y = 2;
 
-print "Problem 1: \ngcd output of $x and $y: ",gcd($x,$y),"\n";
+print "Problem 1: \n output of gcd $x and $y: ",gcd($x,$y),"\n";
 
 
 #problem 2: howmany
 
 sub howmany
 {
-
+    my($fp,@list) = @_;
+    my $result; #variable to hold number
+    foreach my $x (@list)
+    {
+        if ($fp->($x)){$result+=1;}
+    }
+    $result
 }
 
+$fisone = sub
+{
+    my $x = $_[0];
+    if($x == 1){$x}else{0};
+};
 
+@three = (1,0,2,1,4,1);
+
+print "Problem 2:\n";
+print "output of howmany (should be 3): ",howmany($fisone,@three),"\n";
+
+print "output of howmany (should be 1): ",howmany(sub{$_[0] > 3},@three),"\n";
+
+#Problem 3: Toggle closure
+
+sub maketoggle
+{
+    my $x = 0;
+    sub
+    {
+        $_[0]
+    }
+
+}
